4d23886389a6766bb2c31d5f40f7c327b7826bad
aashik-21 <aashikm460@gmail.com>
2026-01-11 13:46:04 +0530

    dasd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react'
 import "./App.css"
 
 function App() {
-return (
+  return (
     <>
-      <div>App</div>
-     </>
-);
-       
+      <div className="quiz-app">
+        <div className="score-section" >
+        <h2>Your Score: 3/3</h2>
+        <button>Restart</button>
+        </div>
+        <div className="question-section">
+          <h2>Question 1</h2>
+          <p>This is sample Question</p>
+          <div className="options">
+            <button>option-1</button>
+            <button>option-2</button>
+            <button>option-3</button>
+            <button>option-4</button>
+          </div>
+          <div className="timer">Time Left:5s</div>
+        </div>
+      </div>
+    </>
+  );
+
 }
 
 export default App;
